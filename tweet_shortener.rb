979025001b3f5934e4b37a033abe437cbaffc3c0
Brianna Dixon <brianna_dixon@rocketmail.com>
2019-01-22 14:39:46 -0500

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.collect {|word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  }.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each {|tweet| puts word_substituter(tweet)}
+end
+
+def selective_tweet_shortener(message)
+  if message.length > 140
+    word_substituter(message)
+  else
+    message
+  end
+end
+
+def shortened_tweet_truncator(message)
+  truncated = word_substituter(message)
+  if truncated.length > 140
+    truncated[0..136] << "..."
+  else
+    truncated
+  end
+end
